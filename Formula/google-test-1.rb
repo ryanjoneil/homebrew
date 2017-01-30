20167028816574bc00530a981bc0740740187827
Ryan J. O'Neil <ryanjoneil@gmail.com>
2017-01-30 13:21:43 -0500

    adding google test

---
+++ Formula/google-test-1.rb
@@ -0,0 +1,14 @@
+class GoogleTest1 < Formula
+  desc "Google Test"
+  homepage "https://github.com/google/googletest"
+  url "https://github.com/google/googletest/archive/release-1.8.0.tar.gz"
+  sha256 "58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8"
+
+  def install
+    system "cmake -DCMAKE_INSTALL_PREFIX:PATH=#{prefix} ."
+    system "make"
+    system "make", "install"
+    lib.install
+    include.install
+  end
+end
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-This tap will give easy access to a few optimization packages through homebrew.
+This tap will give easy access to a few optimization packages and other such thingsthrough homebrew.
 
 ```
 brew tap ryanjoneil/homebrew git@github.com:ryanjoneil/homebrew.git
 brew update
 brew install gecode-5
 brew install gecode-5-doc
+brew install google-test-1
 brew install scip-3
 ```
