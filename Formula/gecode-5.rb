9cb716f9444ee9624ce5cdd4c547bc3a605e94d1
Ryan J. O'Neil <ryanjoneil@gmail.com>
2017-03-09 11:44:20 -0500

    figured out how to build with qt

--- Formula/gecode-5.rb
+++ Formula/gecode-5.rb
@@ -9,7 +9,7 @@ class Gecode5 < Formula
   depends_on "mpfr"
 
   def install
-    system "./configure", "--with-mpfr-lib", "--disable-qt", "--disable-gist", "--disable-examples", "--prefix=#{prefix}"
+    system "./configure", "--with-mpfr-lib", "--disable-examples", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
     lib.install
