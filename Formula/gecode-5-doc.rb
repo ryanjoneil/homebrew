148a7995c6ef2d2d6bc463bad0561bddddeb4cc0
Ryan J. O'Neil <ryanjoneil@gmail.com>
2017-11-03 14:06:03 -0400

    gecode 5.1 xversion bump

--- Formula/gecode-5-doc.rb
+++ Formula/gecode-5-doc.rb
@@ -1,9 +1,9 @@
 class Gecode5Doc < Formula
   desc "Generic Constraint Development Environment: Documentation"
   homepage "http://www.gecode.org/"
-  url "http://www.gecode.org/download/gecode-doc-5.0.0.tar.gz"
-  sha256 "ab3df3be27a4da2f5fbe4e64dc42bc2c7f066a030bdec971f78a9f5c298a5c7a"
-  version "5.0.0"
+  url "http://www.gecode.org/download/gecode-doc-5.1.0.tar.gz"
+  sha256 "d426d0ff16673ab3a0e29db2745b3cf57ce81715c14171e0e480bce55f3c1832"
+  version "5.1.0"
 
   def install
     doc.install Dir["*"]
