9424fdd272b3a01f6beb941be8a6259452f16db4
Ryan J. O'Neil <ryanjoneil@gmail.com>
2018-11-03 15:05:50 -0400

    scip 6

---
+++ Formula/scip-6.rb
@@ -0,0 +1,22 @@
+class Scip5 < Formula
+  desc "SCIP Optimization Suite"
+  homepage "http://scip.zib.de/"
+  url "http://scip.zib.de/download/release/scipoptsuite-5.0.0.tgz"
+  sha256 "af47dd1c28997de857ba54761dc32cbfc54ab739c4050d37efa30cde740baf62"
+  version "5.0.0"
+
+  depends_on "homebrew/science/ipopt"
+  depends_on "zlib"
+
+  depends_on "gmp"
+  depends_on "readline"
+
+  def install
+    system "make", "ZIMPL=true", "SHARED=false"
+    system "make", "install", "ZIMPL=true", "SHARED=false", "INSTALLDIR=#{prefix}"
+
+    bin.install
+    lib.install
+    include.install
+  end
+end
