9310803fa75721f0d19b97abd44068a7878aa67f
Ryan J. O'Neil <ryanjoneil@gmail.com>
2020-02-28 09:52:48 -0800

    adds ipopt, switches to cmake

--- Formula/scip-6.rb
+++ Formula/scip-6.rb
@@ -5,17 +5,23 @@ class Scip6 < Formula
   sha256 "e25329a2ed4fbdde8a32279a4c955ee7a8b8795429b6e7105ae17998c2ecbe66"
   version "6.0.2"
 
+  depends_on "bison"
+  depends_on "flex"
   depends_on "gmp"
   depends_on "ipopt"
+  depends_on "pkg-config"
   depends_on "readline"
   depends_on "zlib"
 
   def install
-    system "make", "ZIMPL=true", "SHARED=false"
-    system "make", "install", "ZIMPL=true", "SHARED=false", "INSTALLDIR=#{prefix}"
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DIPOPT=on", "-DIPOPT_DIR=$(brew --prefix ipopt)/$(brew list --versions ipopt | awk '{print $2}')"
+      system "make"
+      system "make", "install"
 
-    bin.install
-    lib.install
-    include.install
+      bin.install
+      lib.install
+      include.install
+    end
   end
 end
